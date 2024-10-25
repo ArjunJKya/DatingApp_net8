588eaeaf0d663614016b712da8597017a97aeeb6
Arjunkya <armaurya1701@gmail.com>
2024-10-25 23:09:26 +0530

    End Of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -25,6 +26,7 @@ var app = builder.Build();
 // app.UseHttpsRedirection();
 
 // app.UseAuthorization();
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
